56a6d06d6ba077fe2044ba5df9ab742223eaaa57
Dylan Mooney <33304160+dylanmooney62@users.noreply.github.com>
2021-05-20 12:02:42 +0100

    removed box shadow

--- src/components/project/ImageComparison.js
+++ src/components/project/ImageComparison.js
@@ -7,8 +7,6 @@ import Container from "../common/Container";
 
 const css = {
   borderRadius: "0.5rem",
-  boxShadow:
-    "0 10px 15px -3px rgb(0 0 0 / 10%), 0 4px 6px -2px rgb(0 0 0 / 5%)",
 };
 
 const ImageComparison = ({ left, right }) => (
